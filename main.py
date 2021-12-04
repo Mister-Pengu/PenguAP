6f95d1c4db6e42f26733b2f0d2e1174ce45fdab2
Mister-Pengu <95533920+Mister-Pengu@users.noreply.github.com>
2021-12-04 15:58:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+
+import disnake
+import jishaku
+from disnake.ext import commands
+
+from keep_alive import keep_elive
+
+
+bot = commands.Bot(command_prefix="$", Intents=disnake.Intents.all())
+
+jishaku.Flags.NO_UNDERSCORE = True
+jishaku.Flags.NO_DM_TRACEBACK = True
+
+cogs = ["cogs.events", "cogs.commands", "jishaku"]
+
+
+@bot.event
+async def on_ready():
+    for cog in cogs:
+        bot.load_extension(cog)
+
+    print("BOT_CONNECTED")
+
+keep_elive()
+bot.run(os.environ["TOKEN"])
