bbc2895437f1eb977f6c3fdab6b459ba77292399
Mister-Pengu <95533920+Mister-Pengu@users.noreply.github.com>
2021-12-04 15:58:20 +0200

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  return "salo"
+
+def run():
+  app.run("0.0.0.0", 8080)
+
+def keep_elive():
+  t = Thread(target=run)
+  t.start()
