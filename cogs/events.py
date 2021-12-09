3c27a0be2e35e79cb5c7b25b7d1a18dfbd807a57
Mister-Pengu <devlesko1104@gmail.com>
2021-12-09 14:42:53 +0200

    sasd

--- cogs/events.py
+++ cogs/events.py
@@ -7,11 +7,6 @@ class Events(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if "сало" in message.content.lower():
-            await message.reply("смачненьке")
-
 
 def setup(bot):
     bot.add_cog(Events(bot))
